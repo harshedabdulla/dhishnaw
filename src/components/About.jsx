17b7da1bd3856fcc232ec9a7616d15b5e7cc9518
harshedabdulla <harshed2004@gmail.com>
2023-10-01 22:17:25 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,10 +13,10 @@ const Popup = ({ index, title, icon, details, price, register, onRegisterClick }
       <div className="fixed inset-0 bg-black opacity-50"></div>
       <div className="fixed inset-0 flex items-center justify-center z-50">
         <div
-          className="bg-white rounded-lg overflow-hidden shadow-lg relative z-10 w-1/3">
+          className="bg-white rounded-lg overflow-hidden shadow-lg relative z-10 w-full md:w-1/2 lg:w-1/3">
           <div className="p-6">
             <div className="flex justify-between items-center">
-              <h2 className="text-xl text-black font-semibold mb-4">{title}</h2>
+              <h2 className="text-xl md:text-2xl text-black font-semibold mb-4">{title}</h2>
               <button className="text-black text-2xl hover:text-gray-400 transition-all duration-200"
                 onClick={() => {
                   setSelectedId(null);
@@ -37,7 +37,7 @@ const Popup = ({ index, title, icon, details, price, register, onRegisterClick }
             </div>
             <div className="flex justify-between items-center">
               <p className="text-gray-500 text-sm">{price}</p>
-              <button className="bg-[#915eff] text-white text-[16px] font-medium py-2 px-4 rounded-[10px] hover:bg-[#7d4fff] transition-all duration-200"
+              <button className="bg-[#915eff] text-white text-[16px] md:text-base mt-2 font-medium py-2 px-4 rounded-[10px] hover:bg-[#7d4fff] transition-all duration-200"
               >Register</button>
             </div>
           </div>
@@ -50,7 +50,7 @@ const Popup = ({ index, title, icon, details, price, register, onRegisterClick }
 
 const ServiceCard = ({ index, title, icon, details, price, register, onRegisterClick }) => {
   return (
-    <Tilt className="xs:w-[580px] w-full">
+    <Tilt className="md:w-[580px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
@@ -59,13 +59,15 @@ const ServiceCard = ({ index, title, icon, details, price, register, onRegisterC
           scale: 1,
           speed: 450,
         }}
-          className='bg-tertiary rounded-[20px] py-6 px-12 min-h-[380px] flex justify-evenly items-center flex-col'>
-          <img src={icon} alt={title} className='w-20 h-20 object-contain' />
-          <h1 className='text-white text-[20px] font-bold text-center my-4'>{title}</h1>
-          <p className='text-white text-[16px] font-medium text-center'>{details}</p>
-          <div className="flex justify-between w-full mt-2">
-            <p className='text-white text-[16px] font-medium text-center mt-6'>Price: <span className='text-[#915eff]'>{price}</span></p>
-            <button className='bg-[#915eff] text-white text-[16px] mt-2 font-medium py-2 px-4 rounded-[10px] hover:bg-[#7d4fff] transition-all duration-200'
+          className='bg-tertiary rounded-[20px] py-6 px-4 md:px-12 min-h-[380px] flex flex-col justify-between items-center'>
+          <div className="flex flex-col items-center">
+            <img src={icon} alt={title} className='w-20 h-20 object-contain' />
+            <h1 className='text-white text-lg md:text-xl font-bold text-center my-4'>{title}</h1>
+            <p className='text-white text-sm md:text-base font-medium text-center'>{details}</p>
+          </div>
+          <div className="flex justify-between w-full mt-4 md:mt-6">
+            <p className='text-white text-sm md:text-base font-medium text-center'>Price: <span className='text-[#915eff]'>{price}</span></p>
+            <button className='bg-[#915eff] text-white text-[16px] md:text-base mt-2 font-medium py-2 px-4 rounded-[10px] hover:bg-[#7d4fff] transition-all duration-200'
               onClick={() => {
                 onRegisterClick();
               }}>Register</button>
@@ -112,12 +114,12 @@ const About = () => {
       <motion.div>
         <h2 className={`${styles.sectionHeadText} text-center`}>Workshops</h2>
         {/*seach bar */}
-        <div className='w-full flex justify-center items-center gap-4 mt-8'>
-          <input type='text' placeholder='Search' className='w-[400px] h-[50px] rounded-[10px] border-none outline-none px-4 text-[16px] font-medium' onChange={handleValue} />
-          <button className='bg-[#915eff] text-white text-[16px] font-medium py-2 px-4 rounded-[10px] hover:bg-[#7d4fff] transition-all duration-200' onClick={handleSearch}>Search</button>
+        <div className='w-full flex flex-col items-center gap-4 mt-8 md:flex-row md:justify-center md:items-center'>
+          <input type='text' placeholder='Search' className='w-full md:w-[400px] h-[50px] rounded-[10px] border-none outline-none px-4 text-[16px] md:text-base font-medium' onChange={handleValue} />
+          <button className='bg-[#915eff] text-white text-[16px] md:text-base mt-2 md:mt-0 font-medium py-2 px-4 rounded-[10px] hover:bg-[#7d4fff] transition-all duration-200' onClick={handleSearch}>Search</button>
         </div>
       </motion.div>
-      <div className='mt-16 flex flex-wrap gap-16 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8'>
+      <div className='mt-8 md:mt-16 flex flex-wrap gap-4 md:gap-8 xl:gap-16 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8'>
         {data.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service}
             onRegisterClick={() => handleRegisterClick(service.title)}
