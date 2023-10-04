1dd4b7147b18662a3ce7123d442f90e0b42c6667
dhanush <dhanushpk50@gmail.com>
2023-10-04 23:32:45 +0530

    profile changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -29,7 +29,7 @@ const Profile = () => {
   ];
 
   // User profile information (replace with actual user data)
-   const userProfile = {
+  const userProfile = {
     username: 'John Doe',
     email: 'johndoe@example.com',
     phone: '+1 (123) 456-7890',
@@ -46,10 +46,10 @@ const Profile = () => {
       boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Shadow
     },
   };
-  
 
-   // Function to handle sharing on Facebook
-   const shareOnFacebook = () => {
+
+  // Function to handle sharing on Facebook
+  const shareOnFacebook = () => {
     const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
       badgeInfo.badgeImage
     )}&quote=${encodeURIComponent(badgeInfo.badgeText)}`;
@@ -74,94 +74,94 @@ const Profile = () => {
 
   return (
     <div>
-      <Navbar id={id}/>
+      <Navbar id={id} />
       <section
-    className='relative w-full h-auto mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 sm:h-80 rounded-full bg-[#FF884B]' />
-          <div className='w-1 sm:h-80 h-40 violent-gradient' />
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText}`}>Your <span className='text-[#FF884B]'>Profile</span></h1>
-          <div className='my-4'>
-          {/* Social Sharing Buttons */}
-          <button
-            onClick={shareOnFacebook}
-            className='bg-orange-600 text-white px-1 py-1 rounded-xl mr-2 hover:bg-orange-800'
-          >
-            {/** Facebook icon */}
-            <SocialIcon network="facebook" />
-          </button>
-          <button
-            onClick={shareOnTwitter}
-            className='bg-orange-400 text-white px-1 py-1 rounded-xl mr-2 hover:bg-orange-600'
-          >
-            <SocialIcon network="twitter" />
-          </button>
-          <button
-            onClick={shareOnLinkedIn}
-            className='bg-orange-600 text-white px-1 py-1 rounded-xl hover:bg-orange-900'
-          >
-            <SocialIcon network="linkedin" />
-          </button>
-        </div>
+        className='flex flex-wrap w-full h-auto mx-auto'>
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col md:flex-row items-start gap-5`}>
+          <div className='hidden md:flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5  rounded-full bg-[#FF884B]' />
+            <div className='w-1  h-40 violent-gradient' />
+          </div>
+          <div>
+            <h1 className={`${styles.heroHeadText}`}>Your <span className='text-[#FF884B]'>Profile</span></h1>
+            <div className='my-4'>
+              {/* Social Sharing Buttons */}
+              <button
+                onClick={shareOnFacebook}
+                className=' text-white px-1 py-1 rounded-full mr-2 hover:transform hover:scale-110 hover:rotate-12'
+              >
+                {/** Facebook icon */}
+                <SocialIcon network="facebook" />
+              </button>
+              <button
+                onClick={shareOnTwitter}
+                className='text-white px-1 py-1 rounded-full mr-2 hover:transform hover:scale-110 hover:rotate-12'
+              >
+                <SocialIcon network="twitter" />
+              </button>
+              <button
+                onClick={shareOnLinkedIn}
+                className='text-white px-1 py-1 rounded-full mr-2 hover:transform hover:scale-110 hover:rotate-12'
+              >
+                <SocialIcon network="linkedin" />
+              </button>
+            </div>
 
           </div>
           <section className={`${styles.paddingX} mt-10 mx-auto max-w-2xl`}>
-        {/* User Profile */}
-        <Tilt // Wrap the profile card in Tilt component
-  className="Tilt w-full sm:w-auto"
-  options={{ max: 25, scale: 1.1 }} // Tilt options
->
-  <div className="bg-tertiary orange-red-gradient shadow-md p-6 rounded-xl sm:text-left">
-    <div className="flex justify-center sm:justify-between items-center mb-4 mx-4">
-      <h1 className="text-3xl font-bold mb-4">Dhishna 2023</h1>
-      <h2 className="text-xl font-medium mb-4">#45678</h2>
-    </div>
-    <div className="mb-4 text-center">
-      <div className="relative inline-block">
-        <img
-          src="url_to_user_image.jpg" // Replace with the URL to the user's image
-          alt="User Profile"
-          className="w-16 h-16 object-cover rounded-full border-2 border-white shadow-lg"
-        />
-        {/* Add a circular user image */}
-      </div>
-      <p className="text-white my-4">{userProfile.username}</p>
-    </div>
-    <div className="mb-4 text-center">
-      <p className="text-white">{userProfile.email}</p>
-    </div>
+            {/* User Profile */}
+            <Tilt // Wrap the profile card in Tilt component
+              className="Tilt w-full sm:w-auto"
+              options={{ max: 25, scale: 1.1 }} // Tilt options
+            >
+              <div className="bg-tertiary orange-red-gradient shadow-md p-6 rounded-xl sm:text-left">
+                <div className="flex justify-center sm:justify-between items-center mb-4 mx-4">
+                  <h1 className="text-3xl font-bold mb-4">Dhishna 2023</h1>
+                  <h2 className="text-xl font-medium mb-4">#45678</h2>
+                </div>
+                <div className="mb-4 text-center">
+                  <div className="relative inline-block">
+                    <img
+                      src="url_to_user_image.jpg" // Replace with the URL to the user's image
+                      alt="User Profile"
+                      className="w-16 h-16 object-cover rounded-full border-2 border-white shadow-lg"
+                    />
+                    {/* Add a circular user image */}
+                  </div>
+                  <p className="text-white my-4">{userProfile.username}</p>
+                </div>
+                <div className="mb-4 text-center">
+                  <p className="text-white">{userProfile.email}</p>
+                </div>
 
-  </div>
-</Tilt>
-
-
-        {/* Registered Events */}
-        <div className="mt-8">
-          <h1 className="text-2xl font-semibold mb-4">Registered Events</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {registeredEvents.map((event) => (
-              <div
-                key={event.id}
-                className="bg-tertiary shadow-md p-6 rounded-lg"
-              >
-                <h2 className="text-lg font-semibold mb-2">
-                  {event.eventName}
-                </h2>
-                <p className="text-gray-600">
-                  Date: {event.date}<br />
-                  Location: {event.location}
-                </p>
               </div>
-            ))}
-          </div>
+            </Tilt>
+
+
+            {/* Registered Events */}
+            <div className="mt-8">
+              <h1 className="text-2xl font-semibold mb-4">Registered Events</h1>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {registeredEvents.map((event) => (
+                  <div
+                    key={event.id}
+                    className="bg-tertiary shadow-md p-6 rounded-lg"
+                  >
+                    <h2 className="text-lg font-semibold mb-2">
+                      {event.eventName}
+                    </h2>
+                    <p className="text-gray-600">
+                      Date: {event.date}<br />
+                      Location: {event.location}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
         </div>
       </section>
-          </div>
-        </section>
-        <StarsCanvas  />
+      <StarsCanvas />
     </div>
   )
 }
